b120ec6bdb31bcfa5219f1543af949ddee701d6e
Santiago Silva <silvaguarnizo825@gmail.com>
2024-12-14 18:29:59 -0500

    Some changes in the general structure of the project, Subcontratista and Personal entities added.

---
+++ domain/entities/personal.py
@@ -0,0 +1,6 @@
+class Personal:
+    def __init__(self, id, nombre, rol, horas_trabajadas=0):
+        self.id = id
+        self.nombre = nombre
+        self.rol = rol
+        self.horas_trabajadas = horas_trabajadas
---
+++ domain/entities/subcontratista.py
@@ -0,0 +1,6 @@
+class Subcontratista:
+    def __init__(self, id, nombre, especialidad, disponible=True):
+        self.id = id
+        self.nombre = nombre
+        self.especialidad = especialidad
+        self.disponible = disponible
