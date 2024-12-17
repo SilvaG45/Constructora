38e1a4e124cd1bf2ae93bc144846f92f480b7e98
dani-agudelo <daniiagudelom@gmail.com>
2024-12-17 17:51:39 -0500

    solucionando errores

--- app/main.py
+++ app/main.py
@@ -6,16 +6,21 @@ from flask import Flask
 from app.controllers.personal_controller import personal_blueprint
 from app.controllers.subcontratista_controller import subcontratista_blueprint
 from app.controllers.proyecto_controller import proyecto_blueprint
+from app.controllers.cliente_controller import cliente_blueprint
+from app.controllers.proveedor_controller import proveedor_blueprint
+from app.controllers.pedido_controller import pedido_blueprint
+from app.controllers.material_controller import material_blueprint
+from app.controllers.contrato_controller import contrato_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(personal_blueprint, url_prefix="/api/personal")
 app.register_blueprint(subcontratista_blueprint, url_prefix="/api/subcontratista")
 app.register_blueprint(proyecto_blueprint, url_prefix="/api/proyecto")
-app.register_blueprint(proyecto_blueprint, url_prefix="/api/cliente")
-app.register_blueprint(proyecto_blueprint, url_prefix="/api/proveedor")
-app.register_blueprint(proyecto_blueprint, url_prefix="/api/pedido")
-app.register_blueprint(proyecto_blueprint, url_prefix="/api/material")
-app.register_blueprint(proyecto_blueprint, url_prefix="/api/contrato")
+app.register_blueprint(cliente_blueprint, url_prefix="/api/cliente")
+app.register_blueprint(proveedor_blueprint, url_prefix="/api/proveedor")
+app.register_blueprint(pedido_blueprint, url_prefix="/api/pedido")
+app.register_blueprint(material_blueprint, url_prefix="/api/material")
+app.register_blueprint(contrato_blueprint, url_prefix="/api/contrato")
 
 
 
--- domain/entities/proyecto.py
+++ domain/entities/proyecto.py
@@ -1,33 +1,104 @@
 class Proyecto:
     def __init__(self, nombre, presupuesto_inicial, fecha_inicio, fecha_estimacion_fin, porcentaje_avance=0.0):
-        self.nombre = nombre
-        self.presupuesto_inicial = presupuesto_inicial
-        self.fecha_inicio = fecha_inicio
-        self.fecha_estimacion_fin = fecha_estimacion_fin
-        self.porcentaje_avance = porcentaje_avance
-        self.contrato = None  # Ningún contrato asignado inicialmente
-        self.materiales = {}  # Diccionario vacío para materiales
-        self.personal_asignado = {}  # Diccionario vacío para personal
-        self.subcontratistas = {}  # Diccionario vacío para subcontratistas
-        
-    # def actualizar_detalles(self, presupuesto, fecha):
-    #     self.presupuesto_inicial = presupuesto
-    #     self.fecha_estimacion_fin = fecha
+        self._id = None  # No se asigna un id inicialmente
+        self._nombre = nombre
+        self._presupuesto_inicial = presupuesto_inicial
+        self._fecha_inicio = fecha_inicio
+        self._fecha_estimacion_fin = fecha_estimacion_fin
+        self._porcentaje_avance = porcentaje_avance
+        self._contrato = None  # Ningún contrato asignado inicialmente
+        self._materiales = {}  # Diccionario vacío para materiales
+        self._personal_asignado = {}  # Diccionario vacío para personal
+        self._subcontratistas = {}  # Diccionario vacío para subcontratistas
 
-    # def verificar_avance(self):
-    #     return self.porcentaje_avance
+    # Getters y Setters
+    @property
+    def nombre(self):
+        return self._nombre
 
-    # def asignar_personal(self, personal):
-    #     self.personal_asignado.append(personal)
+    @nombre.setter
+    def nombre(self, value):
+        self._nombre = value
 
-    # def asignar_subcontratista(self, subcontratista):
-    #     self.subcontratistas.append(subcontratista)
+    @property
+    def presupuesto_inicial(self):
+        return self._presupuesto_inicial
 
-    # def consultar_materiales_necesarios(self):
-    #     return self.materiales
+    @presupuesto_inicial.setter
+    def presupuesto_inicial(self, value):
+        self._presupuesto_inicial = value
 
-    # def asignar_contrato(self, contrato):
-    #     self.contrato = contrato
-    
+    @property
+    def fecha_inicio(self):
+        return self._fecha_inicio
+
+    @fecha_inicio.setter
+    def fecha_inicio(self, value):
+        self._fecha_inicio = value
+
+    @property
+    def fecha_estimacion_fin(self):
+        return self._fecha_estimacion_fin
+
+    @fecha_estimacion_fin.setter
+    def fecha_estimacion_fin(self, value):
+        self._fecha_estimacion_fin = value
+
+    @property
+    def porcentaje_avance(self):
+        return self._porcentaje_avance
+
+    @porcentaje_avance.setter
+    def porcentaje_avance(self, value):
+        self._porcentaje_avance = value
+
+    @property
+    def contrato(self):
+        return self._contrato
+
+    @contrato.setter
+    def contrato(self, value):
+        self._contrato = value
+
+    @property
+    def materiales(self):
+        return self._materiales
+
+    @materiales.setter
+    def materiales(self, value):
+        self._materiales = value
+
+    @property
+    def personal_asignado(self):
+        return self._personal_asignado
+
+    @personal_asignado.setter
+    def personal_asignado(self, value):
+        self._personal_asignado = value
+
+    @property
+    def subcontratistas(self):
+        return self._subcontratistas
+
+    @subcontratistas.setter
+    def subcontratistas(self, value):
+        self._subcontratistas = value
+
+    # Métodos adicionales
+    def asignar_contrato(self, contrato):
+        self.contrato = contrato
+
+    def agregar_material(self, material_id, cantidad):
+        self.materiales[material_id] = cantidad
+
+    def asignar_personal(self, personal_id, horas):
+        self.personal_asignado[personal_id] = horas
+
+    def asignar_subcontratista(self, subcontratista_id, especialidad):
+        self.subcontratistas[subcontratista_id] = especialidad
+
+    def verificar_avance(self):
+        return self.porcentaje_avance
+
     def __str__(self):
         return f"{self.nombre} - {self.presupuesto_inicial} - {self.fecha_inicio} - {self.fecha_estimacion_fin} - {self.porcentaje_avance}"
