80b94b2ef92edabfb39f0abb806d059cc2ba5f80
Juan Guzman <guzmanhenaojuanesteban@gmail.com>
2024-12-18 22:43:34 -0500

    Organizar el repositorio de inventario con los nombres que son

--- domain/repositories/i_inventario_repository.py
+++ domain/repositories/i_inventario_repository.py
@@ -2,17 +2,29 @@ from abc import ABC, abstractmethod
 
 class IInventarioRepository(ABC):
     @abstractmethod
-    def agregar_material(self, material):
+    def agregar_inventario(self):
         pass
 
     @abstractmethod
-    def obtener_material(self, nombre):
+    def obtener_inventario(self, inventario_id):
         pass
 
     @abstractmethod
-    def actualizar_cantidad(self, nombre, cantidad):
+    def listar_materiales(self):
         pass
 
     @abstractmethod
-    def eliminar_material(self, nombre):
+    def actualizar_inventario(self, nombre, cantidad):
+        pass
+
+    @abstractmethod
+    def registrar_material(self, data):
+        pass
+
+    @abstractmethod
+    def obtener_por_nombre(self, nombre):
+        pass
+
+    @abstractmethod
+    def actualizar_precio(self, nombre, precio):
         pass
